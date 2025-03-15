1c59ffd63abea71ee0ca3d52b88d28f034a93f26
Kaila anderson <kailaamia@gmail.com>
2025-03-15 13:22:15 -0500

    Add password checking method

--- server/db/models/profile.js
+++ server/db/models/profile.js
@@ -3,4 +3,14 @@ const bycrypt = require('bcrypt');
 const sequelize = require('../../config/connection');
 
 
+class Profile extends Model {
+    async checkPasword(loginPassword){
+        try {
+            return await bycrypt.compare(loginPassword, this.password);
+        } catch (err){
+            console.log(err);
+            throw new Error('Error checking password');
+        }
+    };
+};
 
