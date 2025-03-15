d993d216b7efa259599cd47724f0478134fdbd39
Kaila anderson <kailaamia@gmail.com>
2025-03-15 13:22:55 -0500

    Add Profile model with validation and password hashing hooks

--- server/db/models/profile.js
+++ server/db/models/profile.js
@@ -14,3 +14,54 @@ class Profile extends Model {
     };
 };
 
+Profile.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+        unique: true,
+        validate: {
+            len: [3, 30]
+        }
+    },
+    email: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [8]
+        }
+    },
+    subscription: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    }
+},{
+    hooks: {
+        beforeCreate: async (newProfileData) => {
+            newProfileData.password = await bycrypt.hash(newProfileData.password, 10);
+            return newProfileData;
+        },
+        beforeUpdate: async (updatedProfileData) => {
+            updatedProfileData.password = await bycrypt.hash(updatedProfileData.password, 10);
+            return updatedProfileData;
+        },
+
+    },
+    sequelize,
+    updatedAt: false,
+    freezeTableName: true,
+    modelName: 'profile'
+});
